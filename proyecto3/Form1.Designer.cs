fa6a0309a8215651845f8c42c37536f8637189ba
susan <susan@LAPTOP-FSCU55V1>
2024-08-26 14:21:49 -0300

    Agregar archivos de proyecto.

---
+++ proyecto3/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace proyecto3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botonuno = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // botonuno
+            // 
+            this.botonuno.Location = new System.Drawing.Point(565, 99);
+            this.botonuno.Name = "botonuno";
+            this.botonuno.Size = new System.Drawing.Size(75, 23);
+            this.botonuno.TabIndex = 0;
+            this.botonuno.Text = "botonuno";
+            this.botonuno.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.botonuno);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonuno;
+    }
+}
+
