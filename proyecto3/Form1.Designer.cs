d169cf5775931328c003d3c047790676b07d0783
susan <susan@LAPTOP-FSCU55V1>
2024-08-26 14:59:48 -0300

    borre todo

--- proyecto3/Form1.Designer.cs
+++ proyecto3/Form1.Designer.cs
@@ -28,19 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.botonuno = new System.Windows.Forms.Button();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
-            // 
-            // botonuno
-            // 
-            this.botonuno.Location = new System.Drawing.Point(565, 99);
-            this.botonuno.Name = "botonuno";
-            this.botonuno.Size = new System.Drawing.Size(75, 23);
-            this.botonuno.TabIndex = 0;
-            this.botonuno.Text = "botonuno";
-            this.botonuno.UseVisualStyleBackColor = true;
             // 
             // radioButton1
             // 
@@ -53,25 +42,12 @@
             this.radioButton1.Text = "radioButton1";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(415, 213);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(103, 20);
-            this.radioButton2.TabIndex = 3;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.botonuno);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -80,10 +56,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button botonuno;
         private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
